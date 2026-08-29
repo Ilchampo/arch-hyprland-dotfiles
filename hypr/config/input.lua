47d60809603fb6a17943c0e9a013688a407c3aed
Pablo Beltran <beltranflores.juan@gmail.com>
2026-08-28 20:04:05 -0500

    Added input configuration

---
+++ hypr/config/input.lua
@@ -0,0 +1,12 @@
+-- Update with your keyboard layout
+hl.config({
+    input = {
+        kb_layout  = "us",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+        follow_mouse = 1,
+        sensitivity = 0,
+    },
+})
