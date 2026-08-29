10dfb948fe19fc33cb6f654a5a4dc2f1eed9cde3
Pablo Beltran <beltranflores.juan@gmail.com>
2026-08-28 20:01:22 -0500

    Added monitor configuration

---
+++ hypr/config/monitors.lua
@@ -0,0 +1,7 @@
+-- Change output & mode based on what 'hyprctl monitors' return
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "2560x1400@144",
+    position = "0x0",
+    scale    = 1,
+})
