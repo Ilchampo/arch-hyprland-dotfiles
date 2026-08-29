4a075d3a7a260b801e7173d3658732a69661e664
Pablo Beltran <beltranflores.juan@gmail.com>
2026-08-28 20:10:10 -0500

    Added hyprland rules configuration

---
+++ hypr/config/rules.lua
@@ -0,0 +1,28 @@
+-- Ignore applications requests to be maximized
+local suppressMaximizeRule = hl.window_rule({
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+    suppress_event = "maximize",
+})
+
+-- Prevents xwayland stealing keyboard focus
+hl.window_rule({
+    name  = "fix-xwayland-drags",
+    match = {
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
+    },
+    no_focus = true,
+})
+
+-- Forces hyprland-run windows to be floating and centered
+hl.window_rule({
+    name  = "move-hyprland-run",
+    match = { class = "hyprland-run" },
+    move  = "20 monitor_h-120",
+    float = true,
+})
